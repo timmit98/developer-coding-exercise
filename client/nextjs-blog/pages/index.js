8ee17c292e59fb06d93fb6fd792e24f40bd34565
Tim Mitchell <timmitchellnz98@gmail.com>
2024-04-29 20:56:23 +1200

    WIP

--- client/nextjs-blog/pages/index.js
+++ client/nextjs-blog/pages/index.js
@@ -1,52 +1,57 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import {fetchPosts} from '../scripts/fetchPosts';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import { fetchPosts } from "../scripts/fetchPosts";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [JSONPostObject, setposts] = useState({})
+  const [JSONPostObject, setposts] = useState({});
   const router = useRouter();
 
   const handleClick = (postId) => {
-    router.push(`/post/${postId}`);
+    router.push(`/posts/${postId}`);
   };
-  
+
   useEffect(() => {
     const dataFromApiCall = async () => {
-      try 
-      {
+      try {
         const response = await fetchPosts();
         setposts(response);
-      }
-      catch (error) {
-        console.log('Error Fetching Data');
+      } catch (error) {
+        console.log("Error Fetching Data");
       }
     };
-  dataFromApiCall()
+    dataFromApiCall();
   }, []);
 
-  const posts = JSONPostObject.post
-  
+  const posts = JSONPostObject.post;
+
   return (
     <div>
       <Head>
         <title>Blogging Platform</title>
       </Head>
       <main>
-          {posts ? (
-            <ul>
-              {posts.map((post, index) => (
-                <li key={index}>
-                  <Link href={`/post/${post.slug}`}>
-                      <span onClick={() => handleClick(post.id)}>{post.title}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No posts available</p>
-          )}
+        {posts ? (
+          <ul>
+            {posts.map((post, index) => (
+              <li key={index}>
+                <span
+                  onClick={() => handleClick(post.slug)}
+                  style={{
+                    cursor: "pointer",
+                    color: "blue",
+                    textDecoration: "underline",
+                  }}
+                >
+                  {post.title}
+                </span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No posts available</p>
+        )}
       </main>
     </div>
   );
