04dc21b7e7e7cd44a85001738ba52902d0e6b94a
Tim Mitchell <timmitchellnz98@gmail.com>
2024-04-29 20:23:26 +1200

    Skeleton code for the single post page

---
+++ client/nextjs-blog/scripts/fetchPost.js
@@ -0,0 +1,10 @@
+export async function fetchPost(slug)
+{
+	try {
+		const res = await fetch(`http://localhost:3001/posts/${slug}/`); //ideally not hardcoded but needs must 
+		const data = res.json();
+		return data
+	} catch (err) {
+		return {}
+	}
+};
