d4d24f90fc43518e794c5875b1ec66a8b5849ffa
Tim Mitchell <timmitchellnz98@gmail.com>
2024-04-29 20:56:23 +1200

    WORKING SOlUTION:
    Fully working solution

--- client/nextjs-blog/scripts/fetchPost.js
+++ client/nextjs-blog/scripts/fetchPost.js
@@ -1,10 +1,9 @@
-export async function fetchPost(slug)
-{
-	try {
-		const res = await fetch(`http://localhost:3001/posts/${slug}/`); //ideally not hardcoded but needs must 
-		const data = res.json();
-		return data
-	} catch (err) {
-		return {}
-	}
-};
+export async function fetchPost(slug) {
+  try {
+    const res = await fetch(`http://localhost:3001/posts/${slug}/`); //ideally not hardcoded but needs must
+    const data = res.json();
+    return data;
+  } catch (err) {
+    return {};
+  }
+}
